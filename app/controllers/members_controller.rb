3e4a8f8d42df973c41b88d7453d607b5fc09ab6d
giancarloz <gzabaleta48@gmail.com>
2020-04-24 17:41:36 -0400

    set up postgres rails api to send json from frontend to db

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,28 @@
+class MembersController < ApplicationController
+
+    def index
+
+    end
+
+    def show 
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+    private
+
+    def member_params
+    end
+    
+end
